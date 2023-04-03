68c73f1d824ecb22e826a31d6a17faf94831b863
Eman Elbashir <114422580+EmanElbashir@users.noreply.github.com>
2023-04-03 02:59:36 +0200

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,69 @@
+
+// اضافة درك مود 
+let moon = document.querySelector(".fa-moon");
+
+moon.onclick = function () {
+
+    document.body.classList.toggle("dark-mood");
+
+    if (document.body.classList.contains("dark-mood")) {
+        moon.className = "fa-solid fa-sun";
+        localStorage.setItem("them", "dark");
+    }
+    else {
+        moon.className = "fa-regular fa-moon";
+        localStorage.setItem("them", "light");
+    }
+
+}
+
+//لما اعمل ريفيرش للصفحة مود  يتحفظ
+
+if (localStorage.getItem("them") === "dark") {
+    document.body.classList.add("dark-mood");
+    moon.className = "fa-solid fa-sun";
+
+}
+else {
+    document.body.classList.remove("dark-mood");
+    moon.className = "fa-regular fa-moon";
+}
+
+
+
+//typed in sec 2
+var typed = new Typed('.element', {
+    strings: ["EMAN ELBASHIR", "Front end developer "],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+});
+
+
+//arrow in footer
+//whatapp in footer
+let arrow = document.querySelector("#arrow");
+let whatsapp = document.querySelector("#whatsapp");
+
+
+
+arrow.onclick = function () {
+    scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    })
+}
+
+
+window.onscroll = function () {
+    if (scrollY >= 300) {
+        arrow.style.opacity = "1";
+        whatsapp.style.opacity = "1";
+    } else {
+        arrow.style.opacity = "0";
+    }
+
+}
+
+
