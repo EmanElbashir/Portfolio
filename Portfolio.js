cf70d5b3778d5eb8994a1adff4e20ab7074dda91
Eman Elbashir <114422580+EmanElbashir@users.noreply.github.com>
2023-05-15 01:59:11 +0300

    Add files via upload

--- Portfolio.js
+++ Portfolio.js
@@ -56,18 +56,90 @@ arrow.onclick = function() {
 window.onscroll = function() {
     if (scrollY >= 300) {
         arrow.style.opacity = "1";
-        whatsapp.style.opacity = "1";
+      //  whatsapp.style.opacity = "1";
     } else {
         arrow.style.opacity = "0";
     }
 
 }
 
-//From:
 
 
-sendform.addEventListener('submit', (eo) => {
-    eo.preventDefault()();
 
+//firebase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSVpnHP0crqO7hvJw9opRtd7nMX0cifVQ",
+  authDomain: "project-form-b001a.firebaseapp.com",
+  databaseURL: "https://project-form-b001a-default-rtdb.firebaseio.com",
+  projectId: "project-form-b001a",
+  storageBucket: "project-form-b001a.appspot.com",
+  messagingSenderId: "1098558962590",
+  appId: "1:1098558962590:web:d075f6c5a0bf4cd3e9748f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//refrence your database
+
+var contactform = firebase.database().ref('project-form');
+
+document.getElementById("sendform").addEventListener("submit", submitform);
+
+
+function submitform (e){
+  e.preventDefault();
+
+  var namee= getelementvalue("name");
+  var email= getelementvalue("email");
+  var textarea= getelementvalue("textarea");
+
+
+  savemassadge(namee, email, textarea);
+  //enable allert
+
+  document.querySelector(".alert").style.display= 'block';
+
+  setTimeout(function() {
+  
+    document.querySelector(".alert").style.display= 'none';
+  }, 3000);
+
+  //reset form
+  document.getElementById("sendform").reset();
+
+
+}
+
+
+
+const savemassadge= (namee, email, textarea)=>{
+var newcontactform= contactform.push();
+
+newcontactform.set({
+
+namee:namee,
+email: email,
+textarea:textarea,
 
 })
+
+};
+
+
+const getelementvalue=(id)=>{
+return document.getElementById(id).value;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
